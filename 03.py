bfa3e3eea909ab6ad484488c80a8950bdc034199
Takashi Ando <dodo5522@gmail.com>
2021-04-18 11:11:30 +0900

    Implement section 3

---
+++ 03.py
@@ -0,0 +1,111 @@
+from typing import List
+from typing import NewType
+import random
+
+
+Inning = NewType('Inning', int)
+InningFront = Inning(0)
+InningBack = Inning(1)
+
+
+class Team:
+  def __init__(self, name: str, attack: int, defense: int) -> None:
+    self.name = name
+    self.attack = attack
+    self.defense = defense
+    self.total_score = 0
+
+  def info(self) -> str:
+    return '{}: 攻撃力: {} / 守備力: {}'.format(self.name, self.attack, self.defense)
+
+  def get_hit_rate(self) -> int:
+    return random.randint(10, self.attack)
+
+  def get_out_rate(self) -> int:
+    return random.randint(10, self.defense)
+
+  def add_score(self, score):
+    self.total_score += score
+
+
+teams = []
+team_info = [
+  {'name': 'attackers', 'attack': 80, 'defence': 20},
+  {'name': 'defenders', 'attack': 30, 'defence': 70},
+  {'name': 'averages',  'attack': 50, 'defence': 50},
+]
+playing_teams: dict[str, Team] = {
+  'myself': None,
+  'enemy': None,
+}
+
+
+def create_teams() -> None:
+  global teams
+  teams = [Team(i.get('name'), i.get('attack'), i.get('defence')) for i in team_info]
+
+
+def show_teams() -> None:
+  global teams
+  for i, t in enumerate(teams):
+    print('{}. {}'.format(i + 1, t.info()))
+
+
+def choice_team(player: str) -> None:
+  global playing_teams
+  playser_jp = '自分' if player == 'myself' else '相手'
+  num = input('{}のチームを選択してください(1~3): '.format(playser_jp))
+  playing_teams[player] = teams[int(num) - 1]
+  print('{}のチームは「{}」です'.format(playser_jp, playing_teams.get(player).name))
+
+
+def get_play_inning(inning: Inning) -> int:
+  global playing_teams
+  myself = playing_teams.get('myself')
+  enemy = playing_teams.get('enemy')
+  if inning == InningFront:
+    score = (myself.get_hit_rate() - enemy.get_out_rate()) // 10 
+  else:
+    score = (enemy.get_hit_rate() - myself.get_out_rate()) // 10
+  return 0 if score < 0 else score
+
+
+def play() -> None:
+  score_boards = ['____ |', '自分 |', '相手 |']
+
+  create_teams()
+  print('全チームの情報')
+  show_teams()
+  choice_team('myself')
+  choice_team('enemy')
+
+  for number in range(1, 10):
+    score_boards[0] += ' {} |'.format(number)
+  score_boards[0] += ' R |'
+
+  for inning in InningFront, InningBack:
+    for number in range(1, 10):
+      player = list(playing_teams.keys())[inning]
+      if number == 9 and inning == InningBack:
+        myself_total = playing_teams.get('myself').total_score
+        enemy_total = playing_teams.get('enemy').total_score
+        if myself_total > enemy_total:
+          score_boards[inning + 1] += ' X |'
+        else:
+          score = get_play_inning(inning)
+          playing_teams[player].add_score(score)
+          score_boards[inning + 1] += ' {} |'.format(score)
+      else:
+        score = get_play_inning(inning)
+        playing_teams[player].add_score(score)
+        score_boards[inning + 1] += ' {} |'.format(score)
+
+  for i, player in enumerate(playing_teams.keys()):
+      score_boards[i + 1] += ' {} |'.format(playing_teams.get(player).total_score)
+
+  for board in score_boards:
+    print(board)
+
+
+if __name__ == '__main__':
+  play()
