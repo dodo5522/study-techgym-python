e1e796f848100f855e7314883a536c85697a309d
Takashi Ando <dodo5522@gmail.com>
2021-07-15 19:51:22 +0900

    Section 0-15 to show wine data

--- ai/0-12_13_14_15.py
+++ ai/0-12_13_14_15.py
@@ -1,9 +1,14 @@
+import io
 import logging
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import requests
 import seaborn as sns
+from typing import TextIO
+
+
+DB_BASE_URL = 'https://archive.ics.uci.edu/ml/machine-learning-databases'
 
 
 logger = logging.getLogger('main')
@@ -11,29 +16,33 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 
-def get_data(url: str) -> pd.DataFrame:
-  data_set_file = os.path.basename(url)
+def get_data(url: str) -> TextIO:
+  data_file = os.path.basename(url)
 
-  if os.path.isfile(data_set_file):
-    logger.info(f'{data_set_file} already exists')
+  if os.path.isfile(data_file):
+    logger.info(f'{data_file} already exists')
   else:
     res = requests.get(url)
 
     if res.status_code != 200:
       logger.error(res.content.decode('utf-8'))
-      return pd.DataFrame()
+      return ''
 
-    with open(data_set_file, 'w') as fp:
+    with open(data_file, 'w') as fp:
       if fp.write(res.content.decode('utf-8')) <= 0:
-        logger.err(f'Cannot write the below content to {data_set_file}')
+        logger.err(f'Cannot write the below content to {data_file}')
         logger.err(res.content.decode('utf-8'))
-        return pd.DataFrame()
+        return ''
 
-  return pd.read_csv(data_set_file)
+  return io.StringIO(open(data_file, 'r').read())
+
+
+def get_dataframe(url: str, header: bool) -> pd.DataFrame:
+  return pd.read_csv(get_data(url), header=(None if header is False else 0))
 
 
 def adult_stretch() -> None:
-  df = get_data('https://archive.ics.uci.edu/ml/machine-learning-databases/balloons/adult+stretch.data')
+  df = get_dataframe(f'${DB_BASE_URL}/balloons/adult+stretch.data', False)
   df.columns = ['color', 'size', 'act', 'age', 'inflated']
   print(df)
 
@@ -53,7 +62,7 @@ def adult_stretch() -> None:
 
 
 def tic_tac_toe() -> None:
-  df = get_data('https://archive.ics.uci.edu/ml/machine-learning-databases/tic-tac-toe/tic-tac-toe.data')
+  df = get_dataframe(f'${DB_BASE_URL}/tic-tac-toe/tic-tac-toe.data', False)
   df.columns = [
     'top-left-square',
     'top-middle-square',
@@ -78,5 +87,42 @@ def tic_tac_toe() -> None:
   plt.show()
 
 
+def wine() -> None:
+  print(get_data(f'${DB_BASE_URL}/wine/wine.names').read())
+  df = get_dataframe(f'${DB_BASE_URL}/wine/wine.data', False)
+  df.columns = [
+    'class',
+    'Alcohol',
+    'Malic acid',
+    'Ash',
+    'Alcalinity of ash',
+    'Magnesium',
+    'Total phenols',
+    'Flavanoids',
+    'Nonflavanoid phenols',
+    'Proanthocyanins',
+    'Color intensity',
+    'Hue',
+    'OD280/OD315 of diluted wines',
+    'Proline',
+  ]
+  print(df.groupby('class').size())
+  print(df.info())
+  print(df.describe())
+
+  #plt.hist(df.get('Alcohol'), bins=16)
+  #plt.show()
+
+  sns.pairplot(df.get([
+    'class',
+    'Alcohol',
+    'Malic acid',
+    'Ash',
+    'Total phenols',
+    'Color intensity',
+  ]), hue='class', diag_kind='hist')
+  plt.savefig('temp.png')
+
+
 if __name__ == '__main__':
-  tic_tac_toe()
+  wine()
