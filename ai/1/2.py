54a10c742387c1ca3d4284d79271b71d55814038
Takashi Ando <dodo5522@gmail.com>
2021-08-26 21:32:00 +0900

    Section 1-3 to know behaviour of random state and diff b/w make_blob's class and kmeans clustering

--- ai/1/2.py
+++ ai/1/2.py
@@ -9,28 +9,33 @@ from typing import List
 
 def set_layout() -> List[Axes]:
   areas = []
-  f = plt.figure(figsize=[5, 10], dpi=100)
-  gs = f.add_gridspec(2, 1)
-  areas.append(f.add_subplot(gs[0]))
-  areas.append(f.add_subplot(gs[1]))
+  nrows, ncols = 4, 2
+
+  f = plt.figure(figsize=[10, 20], dpi=100)
+  gs = f.add_gridspec(nrows, ncols)
+
+  for i in range(nrows * ncols):
+    areas.append(f.add_subplot(gs[i]))
+
   return areas
 
 
 def main() -> None:
-  data, class_= make_blobs(random_state=5, centers=4)
-  df = pd.DataFrame(data, columns=['x', 'y'])
-
-  model = KMeans(n_clusters=4, random_state=0).fit(df)
-  df['class_kmeans'] = model.predict(df)
-  df['class'] = class_
-
-  print(df)
-
   areas = set_layout()
-  areas[0].set_title('scatter plot')
-  scatterplot(data=df, x='x', y='y', hue='class', ax=areas[0])
-  areas[1].set_title('clustering with KMeans')
-  scatterplot(data=df, x='x', y='y', hue='class_kmeans', ax=areas[1])
+  for i, random_state in enumerate(range(0, 20, 5)):
+    data, class_= make_blobs(random_state=random_state, centers=4)
+    df = pd.DataFrame(data, columns=['x', 'y'])
+
+    print(df)
+
+    model = KMeans(init='random', n_clusters=4).fit(df)
+    df['class_kmeans'] = model.predict(df)
+    df['class'] = class_
+
+    areas[i * 2].set_title(f'scatter plot {random_state}')
+    scatterplot(data=df, x='x', y='y', hue='class', ax=areas[i * 2])
+    areas[i * 2 + 1].set_title(f'clustering with KMeans {random_state}')
+    scatterplot(data=df, x='x', y='y', hue='class_kmeans', ax=areas[i * 2 + 1])
 
   plt.savefig('clustering.png')
 
